d812bda652dd64bdfa950168d171a5ceb0f63481
Diego <diegopablo012@gmail.com>
2025-02-11 17:10:19 -0600

    chore: regex_parser

---
+++ regex_normalizer/tools/regex_parser.py
@@ -0,0 +1,37 @@
+from model.token import Token
+
+def tokenize(regex):
+    """
+    Convierte una expresión regular en una lista de tokens, insertando operadores de concatenación explícitos.
+    
+    Args:
+        regex (str): La expresión regular de entrada.
+    
+    Returns:
+        list: Lista de objetos Token.
+    """
+    tokens = []
+    special_chars = {'(', ')', '*', '+', '?', '|'}
+    previous_token = None
+    
+    for char in regex:
+        if char in special_chars:
+            if char == '(':
+                token = Token('LPAREN', char)
+            elif char == ')':
+                token = Token('RPAREN', char)
+            else:
+                token = Token('OPERATOR', char)
+        else:
+            token = Token('CHAR', char)
+        
+        if previous_token is not None:
+            if (previous_token.type in ['CHAR', 'RPAREN'] or 
+                (previous_token.type == 'OPERATOR' and previous_token.value in ['*', '+', '?'])):
+                if token.type in ['CHAR', 'LPAREN']:
+                    tokens.append(Token('OPERATOR', '.'))
+        
+        tokens.append(token)
+        previous_token = token
+    
+    return tokens
